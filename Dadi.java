c16e0b6ba579a71290759ab273731fa1b14177aa
GIANE <150336285+giane007@users.noreply.github.com>
2025-02-18 12:19:01 +0100

    Update Dadi.java

--- Dadi.java
+++ Dadi.java
@@ -6,37 +6,37 @@ public class Main {
         Scanner tastiera = new Scanner(System.in);
 
         // Messaggi nel main
-        String inserisciGiocatore1 = "Inserisci il nome del Giocatore 1:";
-        String inserisciGiocatore2 = "Inserisci il nome del Giocatore 2:";
-        String inserisciTurni = "Inserisci il numero di turni:";
-        String numeroTurno = "Turno %d";
-        String vincitoreDelTurno = "%s vince il turno!";
-        String parita = "Parità! Entrambi guadagnano un punto.";
-        String risultatiFinali = "\nRisultati finali:";
-        String vincitoreTotTurni = " ha vinto %d turni.";
-        String finePartita = "La partita è finita in parità!";
-        String vincitorePartita = "Il vincitore è: ";
-        String lancioDelDado = "Lancio del dado... ";
+        String messaggioInserisciGiocatore1 = "Inserisci il nome del Giocatore 1:";
+        String messaggioInserisciGiocatore2 = "Inserisci il nome del Giocatore 2:";
+        String messaggioInserisciTurni = "Inserisci il numero di turni:";
+        String messaggioNumeroTurno = "Turno %d";
+        String messaggioVincitoreDelTurno = "%s vince il turno!";
+        String messaggioParita = "Parità! Entrambi guadagnano un punto.";
+        String messaggioRisultatiFinali = "\nRisultati finali:";
+        String messaggioVincitoreTotTurni = " ha vinto %d turni.";
+        String messaggioFinePartita = "La partita è finita in parità!";
+        String messaggioVincitorePartita = "Il vincitore è: ";
+        String messaggioLancioDelDado = "Lancio del dado... ";
 
         // Input giocatori e turni
-        System.out.println(inserisciGiocatore1);
-        String nome1 = tastiera.nextLine();
+        System.out.println(messaggioInserisciGiocatore1);
+        String nomeGiocatore1 = tastiera.nextLine();
 
-        System.out.println(inserisciGiocatore2);
-        String nome2 = tastiera.nextLine();
+        System.out.println(messaggioInserisciGiocatore2);
+        String nomeGiocatore2 = tastiera.nextLine();
 
-        System.out.println(inserisciTurni);
-        int turni = Integer.parseInt(tastiera.nextLine());
+        System.out.println(messaggioInserisciTurni);
+        int numeroTurni = Integer.parseInt(tastiera.nextLine());
 
         // Creazione oggetti per i giocatori e il dado
-        Giocatore giocatore1 = new Giocatore(nome1, 0);
-        Giocatore giocatore2 = new Giocatore(nome2, 0);
+        Giocatore giocatore1 = new Giocatore(nomeGiocatore1, 0);
+        Giocatore giocatore2 = new Giocatore(nomeGiocatore2, 0);
         Dado dado = new Dado(new Random(), 6);
 
         // Inizializzazione della gara
-        Gara gara = new Gara(turni);
-        gara.round(giocatore1, giocatore2, dado, lancioDelDado, numeroTurno, vincitoreDelTurno, parita);
-        gara.risultatoPartita(giocatore1, giocatore2, risultatiFinali, vincitoreTotTurni, finePartita, vincitorePartita);
+        Gara gara = new Gara(numeroTurni);
+        gara.round(giocatore1, giocatore2, dado, messaggioLancioDelDado, messaggioNumeroTurno, messaggioVincitoreDelTurno, messaggioParita);
+        gara.risultatoPartita(giocatore1, giocatore2, messaggioRisultatiFinali, messaggioVincitoreTotTurni, messaggioFinePartita, messaggioVincitorePartita);
     }
 }
 
@@ -52,44 +52,43 @@ public class Gara {
         this.turni = turni;
     }
 
-    public void round(Giocatore g1, Giocatore g2, Dado dado, String diceRollingMessage, String turnMessage, String winnerTurnMessage, String drawMessage) {
+    public void round(Giocatore g1, Giocatore g2, Dado dado, String messaggioLancioDado, String messaggioTurno, String messaggioVincitoreTurno, String messaggioParita) {
         for (int i = 0; i < turni; i++) {
             // Invia al main che è il turno corrente
-            System.out.printf((turnMessage) + "%n", i + 1);
+            System.out.printf((messaggioTurno) + "%n", i + 1);
 
-            int lancio1 = dado.lanciaConAnimazione(diceRollingMessage);
-            int lancio2 = dado.lanciaConAnimazione(diceRollingMessage);
+            int lancio1 = dado.lanciaConAnimazione(messaggioLancioDado);
+            int lancio2 = dado.lanciaConAnimazione(messaggioLancioDado);
 
             // Risultati per il turno
             if (lancio1 > lancio2) {
                 g1.setnVittorie(g1.getnVittorie() + 1);
-                System.out.printf((winnerTurnMessage) + "%n", g1.getNomeGiocatore());
+                System.out.printf((messaggioVincitoreTurno) + "%n", g1.getNomeGiocatore());
             } else if (lancio2 > lancio1) {
                 g2.setnVittorie(g2.getnVittorie() + 1);
-                System.out.printf((winnerTurnMessage) + "%n", g2.getNomeGiocatore());
+                System.out.printf((messaggioVincitoreTurno) + "%n", g2.getNomeGiocatore());
             } else {
                 g1.setnVittorie(g1.getnVittorie() + 1);
                 g2.setnVittorie(g2.getnVittorie() + 1);
-                System.out.println(drawMessage);
+                System.out.println(messaggioParita);
             }
         }
     }
 
-    public void risultatoPartita(Giocatore g1, Giocatore g2, String finalResultsMessage, String winsMessage, String gameOverMessage, String gameWinnerMessage) {
-        System.out.println(finalResultsMessage);
-        System.out.println(g1.getNomeGiocatore() + String.format(winsMessage, g1.getnVittorie()));
-        System.out.println(g2.getNomeGiocatore() + String.format(winsMessage, g2.getnVittorie()));
+    public void risultatoPartita(Giocatore g1, Giocatore g2, String messaggioRisultatiFinali, String messaggioVincitoreTotTurni, String messaggioFinePartita, String messaggioVincitorePartita) {
+        System.out.println(messaggioRisultatiFinali);
+        System.out.println(g1.getNomeGiocatore() + String.format(messaggioVincitoreTotTurni, g1.getnVittorie()));
+        System.out.println(g2.getNomeGiocatore() + String.format(messaggioVincitoreTotTurni, g2.getnVittorie()));
 
         if (g1.getnVittorie() > g2.getnVittorie()) {
-            System.out.println(gameWinnerMessage + g1.getNomeGiocatore());
+            System.out.println(messaggioVincitorePartita + g1.getNomeGiocatore());
         } else if (g2.getnVittorie() > g1.getnVittorie()) {
-            System.out.println(gameWinnerMessage + g2.getNomeGiocatore());
+            System.out.println(messaggioVincitorePartita + g2.getNomeGiocatore());
         } else {
-            System.out.println(gameOverMessage);
+            System.out.println(messaggioFinePartita);
         }
     }
 }
-
 
 
 
@@ -130,10 +129,10 @@ public class Dado {
         this.nFacce = nFacce;
     }
 
-    // Dice animation before the roll
-    public int lanciaConAnimazione(String diceRollingMessage) {
+    // Animazione del lancio del dado
+    public int lanciaConAnimazione(String messaggioLancioDado) {
         // Animazione del dado
-        System.out.print(diceRollingMessage);
+        System.out.print(messaggioLancioDado);
         for (int i = 0; i < 5; i++) {
             try {
                 Thread.sleep(300);  // Tempo tra ogni animazione
@@ -144,9 +143,9 @@ public class Dado {
         }
 
         // Genera il risultato finale
-        int result = valLancio.nextInt(nFacce) + 1;
-        System.out.println("\nLancio: " + result);
-        return result;
+        int risultato = valLancio.nextInt(nFacce) + 1;
+        System.out.println("\nLancio: " + risultato);
+        return risultato;
     }
 }
 
@@ -156,44 +155,45 @@ public class Dado {
 //Package Tools Utility
 package Tools;
 import java.util.Scanner;
-public class Utility{
 
-    private Utility()
-    {}; //impedisce di istanziare la classe
-    public static void clrScr() {
+public class Utility {
+
+    private Utility() {}; // Impedisce di istanziare la classe
+
+    public static void cancellaSchermo() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public static void Wait(int attesa) {
+
+    public static void aspetta(int tempoAttesa) {
         try {
-            Thread.sleep(attesa);
+            Thread.sleep(tempoAttesa);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public static int Menu(String []opzioni, Scanner tastiera) {// parametri formali
+
+    public static int menu(String[] opzioni, Scanner tastiera) {
         int scelta;
 
         do {
-            clrScr();
+            cancellaSchermo();
             System.out.println("------------------");
             System.out.println(opzioni[0]);
             System.out.println("------------------");
             for (int i = 1; i < opzioni.length; i++) {
                 System.out.println("[" + i + "]" + " " + opzioni[i]);
             }
-            scelta = (Integer.parseInt(tastiera.nextLine()));
-            //tastiera.nextLine();
+            scelta = Integer.parseInt(tastiera.nextLine());
             if ((scelta < 1) || (scelta > opzioni.length - 1)) {
                 System.out.println("Opzione Sbagliata");
-                Wait(2000);
+                aspetta(2000);
             }
-        }
-        while ((scelta < 1) || (scelta > opzioni.length - 1));
-        //tastiera.nextLine();
+        } while ((scelta < 1) || (scelta > opzioni.length - 1));
+
         return scelta;
     }
 }
